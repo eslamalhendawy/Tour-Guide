dd8d3960b7a0e4d0dda650aa5a45b45466115ea5
eslam_alhendawy <eslam_alhendawy@outlook.com>
2024-02-03 00:59:02 +0200

    Changed SideMenu

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Drawer from "@mui/material/SwipeableDrawer";
+import Login from "./Login";
 
 function SideMenu() {
   const [openMenu, setOpen] = useState(false);
@@ -13,21 +14,23 @@ function SideMenu() {
     <div className="lg:hidden">
       <i onClick={toggleMenu(true)} className="fa-solid fa-bars text-xl text-white hover:text-primary duration-300 cursor-pointer"></i>
       <Drawer anchor="right" open={openMenu} onClose={toggleMenu(false)} onOpen={toggleMenu(true)}>
-        <div className="w-screen h-full md:max-w-screen-sm flex flex-col justify-between p-3 relative bg-[#1f1f1f]">
+        <div className="w-screen h-full md:max-w-screen-sm flex flex-col justify-between p-3 relative bg-[#505050]">
           <div>
             <div className="text-2xl text-white hover:text-primary duration-300 mb-6">
               <i className="fa-solid fa-x" onClick={toggleMenu(false)}></i>
             </div>
             <ul>
-              <li className="text-white font-semibold text-lg hover:text-primary duration-300 cursor-pointer mb-6">Home</li>
+              <li className="text-white font-semibold text-lg hover:text-primary duration-300 cursor-pointer mb-6">Tours</li>
+              <li className="text-white font-semibold text-lg hover:text-primary duration-300 cursor-pointer mb-6">Packages</li>
+              <li className="text-white font-semibold text-lg hover:text-primary duration-300 cursor-pointer mb-6">Hotels</li>
+              <li className="text-white font-semibold text-lg hover:text-primary duration-300 cursor-pointer mb-6">Search</li>
               <li className="text-white font-semibold text-lg hover:text-primary duration-300 cursor-pointer mb-6">About Us</li>
-              <li className="text-white font-semibold text-lg hover:text-primary duration-300 cursor-pointer mb-6">Tour Packages</li>
               <li className="text-white font-semibold text-lg hover:text-primary duration-300 cursor-pointer mb-6">Contact Us</li>
             </ul>
           </div>
-          <div className=" flex flex-col gap-6 mb-6">
-            <button className="text-white text-lg font-semibold hover:text-primary duration-300">Login</button>
-            <button className="text-white bg-accent hover:bg-secondary duration-300 text-lg font-semibold py-1 px-8 rounded-3xl">Sign Up</button>
+          <div className=" flex gap-6 mb-6">
+            <Login />
+            <button className="bg-brownOrange border border-postage text-postage hover:bg-postage hover:text-white duration-300 text-lg font-base py-1 px-8 rounded-lg">Sign Up</button>
           </div>
         </div>
       </Drawer>
