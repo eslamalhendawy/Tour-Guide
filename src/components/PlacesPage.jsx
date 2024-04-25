0dd02624893169c69a3b092c497416e688f9278c
eslam_alhendawy <eslam_alhendawy@outlook.com>
2024-04-25 09:33:54 +0200

    Finished PlacesPage pagination

--- src/components/PlacesPage.jsx
+++ src/components/PlacesPage.jsx
@@ -2,29 +2,40 @@ import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 import { getData } from "../Services/APICalls";
 import { Link } from "react-router-dom";
+import ReactPaginate from "react-paginate";
 
 const PlacesPage = () => {
   const [loading, setLoading] = useState(true);
   const [articles, setArticles] = useState([]);
+  const [numberPages, setNumberPages] = useState(0);
+  const [currentPage, setCurrentPage] = useState(1);
 
   const { t, i18n } = useTranslation();
   const selectedLanguage = i18n.language;
 
   useEffect(() => {
-    getData("place")
+    getData(`place?page=${currentPage}`)
       .then((response) => {
+        console.log(response);
         setArticles(response.data.places);
+        setNumberPages(response.numberOfPages);
         setLoading(false);
       })
       .catch((error) => {
         console.log(error);
       });
-  }, []);
+  }, [currentPage]);
 
   useEffect(() => {
     document.title = "Egytravler | Places";
     window.scrollTo(0, 0);
-  }, []);
+  }, [currentPage]);
+
+
+  const handlePageChange = (selectedPage) => {
+    setCurrentPage(selectedPage.selected + 1);
+    window.scrollTo(0, 0);
+  };
 
   return (
     <div className="bg-postage">
@@ -33,24 +44,31 @@ const PlacesPage = () => {
         {loading ? (
           <p className="text-white text-xl font-semibold">Loading...</p>
         ) : (
-          <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
-            {articles.map((article) => (
-              <Link to={`/place/${article._id}`} key={article._id}>
-                <div className="flex flex-col gap-4 bg-white p-2 rounded-lg mb-4">
-                  <div className="max-w-[300px] h-[175px] mx-auto">
-                    <img src={article.image} alt="" className="w-full h-full object-cover rounded-lg" />
+          <>
+            <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
+              {articles.map((article) => (
+                <Link to={`/place/${article._id}`} key={article._id}>
+                  <div className="flex flex-col gap-4 bg-white p-2 rounded-lg mb-4">
+                    <div className="max-w-[300px] h-[175px] mx-auto">
+                      <img src={article.image} alt="" className="w-full h-full object-cover rounded-lg" />
+                    </div>
+                    <div className="flex flex-col gap-2">
+                      <h3 className="text-brownOrange text-center text-lg font-bold">{article.name}</h3>
+                    </div>
                   </div>
-                  <div className="flex flex-col gap-2">
-                    <h3 className="text-brownOrange text-center text-lg font-bold">{article.name}</h3>
-                  </div>
-                </div>
-              </Link>
-            ))}
-          </div>
+                </Link>
+              ))}
+            </div>
+            <div className="flex items-center justify-center my-8">
+            <ReactPaginate className="text-white font-bold flex gap-2 text-xl " breakLabel="..." nextLabel=">" pageCount={numberPages} pageRangeDisplayed={1} previousLabel="<" onPageChange={handlePageChange} activeClassName={"active"} containerClassName={"pagination"} />
+            </div>
+          </>
         )}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default PlacesPage
+// onPageChange={handlePageChange}
+
+export default PlacesPage;
